f0b4c880c34e9120b9e5da6acf347079e425f8ab
AnojAryal <anoj1810@gmail.com>
2024-09-12 16:58:15 +0545

    model setup

---
+++ models/product.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Product struct {
+	ID           uint `json:"id" gorm:"primarykey"`
+	CreatedAt    time.Time
+	Name         string `json:"name"`
+	SerialNumber string `json :"serialnumber"`
+}
