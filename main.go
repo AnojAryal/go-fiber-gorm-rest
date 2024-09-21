3892687ae9fa09c374b931ae317bb7497498878b
AnojAryal <anoj1810@gmail.com>
2024-09-21 11:07:02 +0545

    product route and create products

--- main.go
+++ main.go
@@ -21,11 +21,14 @@ func welcome(c *fiber.Ctx) error {
 func setupRoutes(app *fiber.App) {
 	//welcome api
 	app.Get("/api", welcome)
+	//user routes
 	app.Post("/api/create-user", routes.CreateUser)
 	app.Get("/api/users", routes.GetUser)
 	app.Get("/api/users/:id", routes.GetUserById)
 	app.Put("/api/update-user/:id", routes.UpdateUser)
 	app.Delete("/api/delete-user/:id", routes.DeleteUser)
+	//product routes
+	app.Post("/api/products", routes.CreateProduct)
 }
 
 func main() {
